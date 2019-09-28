afa4a2d0841c41d329a682c84a3734dccf34737e
amit2git <amitsingh180223@gmail.com>
2019-09-28 17:21:17 +0530

    hi

---
+++ src/main/java/com/example/MyProject1/MyProject1Application.java
@@ -0,0 +1,14 @@
+package com.example.MyProject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyProject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyProject1Application.class, args);
+	}
+
+	System.out.println("Hello World!");
+}
